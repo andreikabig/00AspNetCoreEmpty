a48aaf96c93d8a60c03f30d9b1d744fa7741e73b
Andrey Dyudkin <apps.bigint@gmail.com>
2022-01-06 01:29:12 +0300

    Create Class TokenMiddleware

---
+++ 00AspNetCoreEmpty/Middlewares/TokenMiddleware.cs
@@ -0,0 +1,34 @@
+﻿namespace _00AspNetCoreEmpty.Middlewares
+{
+    // MiddleWare отдельным классом
+    
+    /*
+        ТРЕБОВАНИЯ
+        ----------
+        1. Конструктор, который принимает параметр типа RequestDelegate - через этот параметр можно получить ссылку на тот делегат запроса, который стоит следующим
+                                                                          в ковейере обработки запроса
+        2. Метод Invoke (либо InvokeAsync) - возвращает Task и принимает HttpContext; данный метод обрабатывает наш запрос
+     
+     */
+    public class TokenMiddleware
+    {
+        private readonly RequestDelegate next;
+        public TokenMiddleware(RequestDelegate next)
+        {
+            this.next = next;   
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            var token = context.Request.Query["token"];
+            if (token != "12345678")
+            {
+                context.Response.ContentType = "text/html; charset=UTF-8";
+                context.Response.StatusCode = 403;
+                await context.Response.WriteAsync("Неправильный token!");
+            }
+            else
+                await next.Invoke(context);
+        }
+    }
+}
--- 00AspNetCoreEmpty/Program.cs
+++ 00AspNetCoreEmpty/Program.cs
@@ -15,6 +15,7 @@ using System.Text;
 using System.Text.Json;
 using System.Text.RegularExpressions;
 using System.Linq;
+using _00AspNetCoreEmpty.Middlewares;
 
 // Создаем объект builder приложения WebApplicationBuilder
 var builder = WebApplication.CreateBuilder(args);
@@ -22,64 +23,13 @@ var builder = WebApplication.CreateBuilder(args);
 // Создаем объект WebApplication на основе билдера
 var app = builder.Build();
 
-/*
-    .Map() - создание ветки конвейера, которая обрабатывает запрос по определенному пути
-        
-        Параметры:
-                    1. Путь запроса, с которым сопоставляется ветка
-                    2. делегат Action<IApplicationBuilder> - действия над объектом IApplicationBuilder, в котором создается ветка конвейера
-       
- */
+app.UseMiddleware<TokenMiddleware>();
 
-app.Map("/time", appBuilder =>
-    {
-        var time = DateTime.Now.ToShortTimeString();
-
-        appBuilder.Use(async (context, next) =>
-        {
-            Console.WriteLine(time);
-            await next.Invoke();
-        });
-
-        appBuilder.Run(async (context) =>
-        {
-            await context.Response.WriteAsync(time);
-        });
-    });
-
-// Примеры вложенных методов Map
-app.Map("/home", appBuilder =>
-{
-    // /home/index
-    appBuilder.Map("/index", IndexPage);
-    appBuilder.Map("/about", AboutPage);
-
-    // /home
-    appBuilder.Run(async (context) => await context.Response.WriteAsync("Home page")) ;
-
-});
-
-// Ветки для разных путей
-// app.Map("/index", ...)
-
-app.Run(async (context) =>
-{
-    await context.Response.WriteAsync("Welcome to RubyOnBrain!");
-});
-
+app.Run(async (context) => {
+    context.Response.ContentType = "text/html; charset=UTF-8";
+    await context.Response.WriteAsync("Успешный вход по токену на сайт!"); });
 
 app.Run();
-
-
-void IndexPage(IApplicationBuilder appBuilder)
-{
-    appBuilder.Run(async context => await context.Response.WriteAsync("Index page"));
-}
-
-void AboutPage(IApplicationBuilder appBuilder)
-{
-    appBuilder.Run(async context => await context.Response.WriteAsync("About page"));
-}
 
 
 
